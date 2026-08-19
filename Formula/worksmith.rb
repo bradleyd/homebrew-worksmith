62ed8470bd307a359913629985ceefc6ae9d8fb4
Bradley Smith <bradleydsmith@gmail.com>
2026-08-19 17:55:03 -0600

    worksmith 0.1.0: real checksums, drop Intel macOS, add Linux musl

--- Formula/worksmith.rb
+++ Formula/worksmith.rb
@@ -5,14 +5,12 @@ class Worksmith < Formula
   license "MIT"
 
   on_macos do
-    on_arm do
-      url "https://github.com/bradleyd/worksmith/releases/download/v0.1.0/worksmith-0.1.0-aarch64-apple-darwin.tar.gz"
-      sha256 "REPLACE_WITH_SHA256"
-    end
-    on_intel do
-      url "https://github.com/bradleyd/worksmith/releases/download/v0.1.0/worksmith-0.1.0-x86_64-apple-darwin.tar.gz"
-      sha256 "REPLACE_WITH_SHA256"
-    end
+    url "https://github.com/bradleyd/worksmith/releases/download/v0.1.0/worksmith-0.1.0-aarch64-apple-darwin.tar.gz"
+    sha256 "6ff32b4beedf0d242959f734c2e776fd9b69484421d6419508b5c70dd60110d6"
+  end
+  on_linux do
+    url "https://github.com/bradleyd/worksmith/releases/download/v0.1.0/worksmith-0.1.0-x86_64-unknown-linux-musl.tar.gz"
+    sha256 "185ef264bd6ad274022548dc99607f4277b1c330405108de4f860bf06130da86"
   end
 
   def install
