e85bc6878cbb82c2b74f4103595b73df587a50ef
Bradley Smith <bradleydsmith@gmail.com>
2026-08-19 17:58:47 -0600

    macOS-only: drop on_linux block (Linux users take the release tarball)

--- Formula/worksmith.rb
+++ Formula/worksmith.rb
@@ -8,10 +8,6 @@ class Worksmith < Formula
     url "https://github.com/bradleyd/worksmith/releases/download/v0.1.0/worksmith-0.1.0-aarch64-apple-darwin.tar.gz"
     sha256 "6ff32b4beedf0d242959f734c2e776fd9b69484421d6419508b5c70dd60110d6"
   end
-  on_linux do
-    url "https://github.com/bradleyd/worksmith/releases/download/v0.1.0/worksmith-0.1.0-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "185ef264bd6ad274022548dc99607f4277b1c330405108de4f860bf06130da86"
-  end
 
   def install
     bin.install "worksmith"
